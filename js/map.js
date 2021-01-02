75f29f37dd6bc9c4e3344b143126a3ac3fc2356b
cherry13579 <38655089+cherry13579@users.noreply.github.com>
2021-01-02 18:49:09 +0100

    Update map.js
    adding pop ups

--- js/map.js
+++ js/map.js
@@ -20,6 +20,7 @@ var TreeCadSource = new ol.source.Vector({
       src: 'img/tree.png',
     }),
   });
+ 
   
   var clusterSource = new ol.source.Cluster({
     distance: 20,
@@ -62,9 +63,10 @@ var TreeCadSource = new ol.source.Vector({
     //style: iconStyle
   });
 
+  /* Map View and zoom changed */
   var view = new ol.View({
-    center: ol.proj.transform([7.62869,51.9629],'EPSG:4326','EPSG:3857'),
-    zoom: 18,
+    center: ol.proj.transform([7.625868962895809,51.96263192130524],'EPSG:4326','EPSG:3857'),
+    zoom: 19,
   });
 
  var osm = new ol.layer.Tile({
@@ -72,7 +74,65 @@ var TreeCadSource = new ol.source.Vector({
   });
 
   var map = new ol.Map({
-    layers: [ osm, TreeCad ],
+    layers: [ osm ],
     target: 'map',
     view: view,
+    interactions: ol.interaction.defaults({ altShiftDragRotate:false, pinchRotate:false }),
   });
+
+  map.addLayer(TreeCad);
+
+ 
+
+ /*popup*/
+  var select = new ol.interaction.Select({
+    hitTolerance: 5,
+    multi: true,
+    condition: ol.events.condition.singleClick
+  });
+  map.addInteraction(select);
+
+  var popup = new ol.Overlay.PopupFeature({
+    popupClass: 'default anim',
+    select: select,
+    canFix: true,
+    template: {
+        
+        title: 'Title',
+        attributes: // [ 'baumgruppe', 'str_schl' ]
+        {  
+          'str_schl': { title: 'Straßenschlüssel' },
+          'baumgruppe': { title: 'Species' },
+         
+         
+          
+          
+          
+          
+          /* Using localString with a date * /
+          'date': { 
+            title: 'Date', 
+            format: ol.Overlay.PopupFeature.localString(undefined, { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }) 
+          }
+          /**/
+        }
+    }
+  });
+  map.addOverlay (popup);
+
+
+/*
+  var popup = new ol.Overlay.Popup();
+  map.addOverlay(popup);
+  
+  map.on('click', function(evt) {
+      var prettyCoord = ol.TreeCad.toStringHDMS(ol.proj.transform(evt.coordinate, 'EPSG:3857', 'EPSG:4326'), 2);
+      popup.show(evt.coordinate, '<div><h2>Coordinates</h2><p>' + prettyCoord + '</p></div>');
+  });
+  */
+
+
+
+    
+
+
